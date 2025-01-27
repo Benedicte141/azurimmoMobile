ef8a4226353666b7ea5d18cbd24cd2a755ccde20
benedicte.gady <benedicte.gady@sts-sio-caen.info>
2025-01-27 16:52:41 +0100

    Modification BatimentViewModel et AndroidManifest

--- app/src/main/java/bts/sio/azurimmo/viewsmodel/appartement/AppartementViewModel.kt
+++ app/src/main/java/bts/sio/azurimmo/viewsmodel/appartement/AppartementViewModel.kt
@@ -5,25 +5,24 @@ import androidx.lifecycle.ViewModel
 import bts.sio.azurimmo.model.Appartement
 import androidx.compose.runtime.*
 import androidx.lifecycle.viewModelScope
+import bts.sio.azurimmo.api.RetrofitInstance
 import kotlinx.coroutines.launch
 
 // ViewModel pour gérer les données des appartements
 class AppartementViewModel : ViewModel() {
-    // Liste mutable des appartements
-    private val _appartements = mutableStateOf(emptyList<Appartement>())
-    val appartements: State<List<Appartement>> = _appartements
-    init {
-// Simuler un chargement de données initiales
-        getAppartements()
-    }
+    // Liste mutable des appartements ===> "Données en dur"
+    //private val _appartements = mutableStateOf(emptyList<Appartement>())
+    //val appartements: State<List<Appartement>> = _appartements
+    //init {
+    // Simuler un chargement de données initiales
+    //    getAppartements()
+    //}
     // Fonction pour simuler le chargement de appartements
-    private fun getAppartements() {
-        viewModelScope.launch {
-            _appartements.value = listOf(
-                Appartement(1, 123, 20.0, nbrePieces = 2, description = "Studio"),
-                Appartement(2, 30, 15.0, nbrePieces = 3, description = "Duplex"),
-                Appartement(3, 45, 45.0, nbrePieces = 5, description = "Appartement face à la mer")
-            )
-        }
-    }
+    //private fun getAppartements() {
+    //    viewModelScope.launch {
+    //        _appartements.value = listOf(
+    //            Appartement(1, 123, 20.0, nbrePieces = 2, description = "Studio"),
+    //            Appartement(2, 30, 15.0, nbrePieces = 3, description = "Duplex"),
+    //            Appartement(3, 45, 45.0, nbrePieces = 5, description = "Appartement face à la mer")
+    //        )
 }
