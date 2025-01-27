ec80ab28dedcb90f0e98cb953b3f569f1f15d22a
benedicte.gady <benedicte.gady@sts-sio-caen.info>
2025-01-27 15:52:42 +0100

    Création de la Classe AppartementCard

--- app/src/main/java/bts/sio/azurimmo/viewsmodel/appartement/AppartementViewModel.kt
+++ app/src/main/java/bts/sio/azurimmo/viewsmodel/appartement/AppartementViewModel.kt
@@ -1,4 +1,29 @@
 package bts.sio.azurimmo.viewsmodel.appartement
 
-class AppartementViewModel {
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import bts.sio.azurimmo.model.Appartement
+import androidx.compose.runtime.*
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+// ViewModel pour gérer les données des appartements
+class AppartementViewModel : ViewModel() {
+    // Liste mutable des appartements
+    private val _appartements = mutableStateOf(emptyList<Appartement>())
+    val appartements: State<List<Appartement>> = _appartements
+    init {
+// Simuler un chargement de données initiales
+        getAppartements()
+    }
+    // Fonction pour simuler le chargement de appartements
+    private fun getAppartements() {
+        viewModelScope.launch {
+            _appartements.value = listOf(
+                Appartement(1, 123, 20.0, nbrePieces = 2, description = "Studio"),
+                Appartement(2, 30, 15.0, nbrePieces = 3, description = "Duplex"),
+                Appartement(3, 45, 45.0, nbrePieces = 5, description = "Appartement face à la mer")
+            )
+        }
+    }
 }
