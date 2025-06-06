1ffe5024b2353b6d6f5c24364b71d61a0651884c
benedicte.gady <benedicte.gady@sts-sio-caen.info>
2025-06-06 16:13:11 +0200

    Fin

--- app/src/main/java/bts/sio/azurimmo/api/RetrofitInstance.kt
+++ app/src/main/java/bts/sio/azurimmo/api/RetrofitInstance.kt
@@ -2,17 +2,17 @@ package bts.sio.azurimmo.api
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import bts.sio.azurimmo.config.NetworkConfig
+
 
 
 object RetrofitInstance {
-    /*private const val BASE_URL = "http://10.0.2.2:9008/" */
-    /*private const val BASE_URL = "http://192.168.137.1:9008/" */
+    private const val BASE_URL = "http://10.0.2.2:9008/"
+   /*private const val BASE_URL = "http://192.168.10.100:9008/" */
 
 
     val api: ApiService by lazy {
         Retrofit.Builder()
-            .baseUrl(NetworkConfig.getBaseUrl())
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
