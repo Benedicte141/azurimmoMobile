6564c25139097f75f2d7148d43d4694e02a56912
benedicte.gady <benedicte.gady@sts-sio-caen.info>
2025-06-06 11:45:20 +0200

    Modif fichier pour connexion true/false URL emulateur

--- app/src/main/java/bts/sio/azurimmo/api/RetrofitInstance.kt
+++ app/src/main/java/bts/sio/azurimmo/api/RetrofitInstance.kt
@@ -2,16 +2,17 @@ package bts.sio.azurimmo.api
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import bts.sio.azurimmo.config.NetworkConfig
 
 
 object RetrofitInstance {
     /*private const val BASE_URL = "http://10.0.2.2:9008/" */
-    private const val BASE_URL = "http://192.168.137.1:9008/"
+    /*private const val BASE_URL = "http://192.168.137.1:9008/" */
 
 
     val api: ApiService by lazy {
         Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(NetworkConfig.getBaseUrl())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
